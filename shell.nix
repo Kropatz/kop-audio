030dc41d7080544734bd5f098515bbc6800dd137
Kopatz <7265381+Kropatz@users.noreply.github.com>
2025-11-30 11:04:53 +0100

    audio encoding

--- shell.nix
+++ shell.nix
@@ -10,22 +10,13 @@ pkgs.mkShell rec {
   ];
 
   nativeBuildInputs = with pkgs; [
+    libopus
     libpulseaudio
   ];
 
   shellHook =
     let
       libraries = with pkgs; [
-
-        libGL
-        # X11 dependencies
-        xorg.libX11
-        xorg.libX11.dev
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXinerama
-        xorg.libXrandr
-        libpulseaudio
       ];
     in
     ''
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::error::Error;
-use std::fs::OpenOptions;
-use std::io::Write;
-
-use libpulse_binding::def::BufferAttr;
 use libpulse_binding as pulse;
+use libpulse_binding::def::BufferAttr;
 use libpulse_simple_binding as psimple;
+use opus::Application::Voip;
+use opus::{Channels, Decoder, Encoder};
 use psimple::Simple;
 use pulse::sample::{Format, Spec};
 use pulse::stream::Direction;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::slice;
+
+const SAMPLE_RATE: u32 = 48000;
+const CHANNELS: usize = 2;
+const BUF_SIZE: u32 = 3840; // 20ms of stereo 48kHz 16-bit audio = 48000 samples/sec * 0.02 sec * 2 channels * 2 bytes/sample = 3840 bytes
+const FRAME_SIZE: usize = 960; // for opus - 20ms at 48kHz. Per channel, so total samples = FRAME_SIZE * CHANNELS = 1920
 
 #[derive(Debug)]
 enum ErrorKind {
@@ -60,36 +66,35 @@ fn main() {
     let mut file_consumer = FileConsumer::new("output.pcm").unwrap();
     let spec = Spec {
         format: Format::S16NE,
-        channels: 2,
-        rate: 44100,
+        channels: CHANNELS as u8,
+        rate: SAMPLE_RATE,
     };
     // https://www.freedesktop.org/software/pulseaudio/doxygen/structpa__buffer__attr.html#abef20d3a6cab53f716846125353e56a4
-    const BUF_SIZE: u32 = 4096;
     let record_attr = BufferAttr {
         maxlength: u32::MAX, // maximum length of the buffer
-        tlength:   u32::MAX, // playback-only: target length of the buffer
-        prebuf:    u32::MAX, // playback-only: prebuffering size
-        minreq:    u32::MAX, // minimum request size
-        fragsize: BUF_SIZE, // record-only: fragment size
+        tlength: u32::MAX,   // playback-only: target length of the buffer
+        prebuf: u32::MAX,    // playback-only: prebuffering size
+        minreq: u32::MAX,    // minimum request size
+        fragsize: BUF_SIZE,  // record-only: fragment size
     };
     let playback_attr = BufferAttr {
-        maxlength: u32::MAX, // maximum length of the buffer
-        tlength:   BUF_SIZE * 3, // playback-only: target length of the buffer
-        prebuf:    BUF_SIZE * 2, // playback-only: prebuffering size
-        minreq:    BUF_SIZE, // minimum request size
-        fragsize: u32::MAX, // record-only: fragment size
+        maxlength: u32::MAX,   // maximum length of the buffer
+        tlength: BUF_SIZE * 3, // playback-only: target length of the buffer
+        prebuf: BUF_SIZE * 2,  // playback-only: prebuffering size
+        minreq: BUF_SIZE,      // minimum request size
+        fragsize: u32::MAX,    // record-only: fragment size
     };
     assert!(spec.is_valid());
 
     let rec = Simple::new(
-        None,              // Use the default server
-        "Rustaudio Recorder",        // Our application’s name
-        Direction::Record, // We want a recording stream
-        None,              // Use the default device
-        "Record",          // Description of our stream
-        &spec,             // Our sample format
-        None,              // Use default channel map
-        Some(&record_attr),              // Use default buffering attributes
+        None,                 // Use the default server
+        "Rustaudio Recorder", // Our application’s name
+        Direction::Record,    // We want a recording stream
+        None,                 // Use the default device
+        "Record",             // Description of our stream
+        &spec,                // Our sample format
+        None,                 // Use default channel map
+        Some(&record_attr),   // Use default buffering attributes
     )
     .unwrap();
     let out = Simple::new(
@@ -105,11 +110,16 @@ fn main() {
     .unwrap();
     let mut audio_consumer = AudioConsumer { endpoint: out };
     let consumers: &mut [&mut dyn Consumer] = &mut [&mut audio_consumer];
-    let mut data = [0u8; BUF_SIZE as usize];
+
+    let mut data = vec![0u8; BUF_SIZE as usize];
+    let mut encoded_data = [0u8; BUF_SIZE as usize];
+    let mut decoded_data = vec![0i16; FRAME_SIZE * CHANNELS];
+    let mut encoder = opus_encoder();
+    let mut decoder = opus_decoder();
     loop {
         match rec.get_latency() {
             Ok(latency) => {
-                println!("Current latency: {} ms", latency.as_millis());
+                println!("Latency: {} ms", latency.as_millis());
             }
             Err(e) => {}
         }
@@ -120,11 +130,43 @@ fn main() {
                 break;
             }
         }
-        consumers.iter_mut().for_each(|c| match c.consume(&data) {
-            Ok(_) => {}
-            Err(e) => {
-                eprintln!("Error consuming data: {:?}", e);
+
+        let pcm: &[i16] =
+            unsafe { slice::from_raw_parts(data.as_ptr() as *const i16, data.len() / 2) };
+
+        let samples_needed = FRAME_SIZE * CHANNELS;
+        let pcm = &pcm[..samples_needed];
+        let n = encoder.encode(&pcm, &mut encoded_data).unwrap();
+        let b = decoder
+            .decode(&encoded_data[..n], &mut decoded_data, false)
+            .unwrap();
+
+        println!(
+            "Read {} samples, data has {} samples, encoded to {} bytes, decoded to {} samples",
+            pcm.len(),
+            data.len() / 2,
+            n,
+            b
+        );
+        consumers.iter_mut().for_each(|c| {
+            match c.consume(unsafe {
+                slice::from_raw_parts(
+                    decoded_data.as_ptr() as *const u8,
+                    b * CHANNELS * std::mem::size_of::<i16>(),
+                )
+            }) {
+                Ok(_) => {}
+                Err(e) => {
+                    eprintln!("Error consuming data: {:?}", e);
+                }
             }
         });
     }
+}
+
+fn opus_encoder() -> Encoder {
+    Encoder::new(SAMPLE_RATE, Channels::Stereo, Voip).unwrap()
+}
+fn opus_decoder() -> Decoder {
+    Decoder::new(SAMPLE_RATE, Channels::Stereo).unwrap()
 }
